2d9d30cc339db7a66b298fb4a85ffd9c97171547
Daniel Guerra <danielguerra.g12@gmail.com>
2020-08-22 04:03:24 -0300

    Criei métodos de CRUD retornando corretamente os erros de acordo com sua ação.

--- src/main/java/com/algaworks/osworks/domain/repository/ClienteRepository.java
+++ src/main/java/com/algaworks/osworks/domain/repository/ClienteRepository.java
@@ -1,5 +1,7 @@
 package com.algaworks.osworks.domain.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.algaworks.osworks.domain.models.Cliente;
@@ -7,4 +9,5 @@ import com.algaworks.osworks.domain.models.Cliente;
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Long>{
 
+	List<Cliente> findByNome(String nome);
 }
