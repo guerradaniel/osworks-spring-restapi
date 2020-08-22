1b5a107fa241dc5c187af9a99618f2eae1127e44
Daniel Guerra <danielguerra.g12@gmail.com>
2020-08-22 02:22:33 -0300

    Criei interface para separar as responsabilidades do ClienteController.

--- src/main/java/com/algaworks/osworks/api/controllers/ClienteController.java
+++ src/main/java/com/algaworks/osworks/api/controllers/ClienteController.java
@@ -1,35 +1,26 @@
 package com.algaworks.osworks.api.controllers;
 
-import java.util.Arrays;
 import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.algaworks.osworks.domain.models.Cliente;
+import com.algaworks.osworks.domain.repository.ClienteRepository;
 
 
 @RestController
 public class ClienteController {
 	
 	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
-		var cliente1 = new Cliente();
-		cliente1.setId(1L);
-		cliente1.setNome("Daniel Guerra");
-		cliente1.setEmail("daniel@akgaworks.com.br");
-		cliente1.setTelefone("11 99991-5472");
-		
-		var cliente2 = new Cliente();
-		cliente2.setId(2L);
-		cliente2.setNome("Thiago");
-		cliente2.setEmail("thiago@algaworks.com.br");
-		cliente2.setTelefone("11 99293-5353");
-		
-		
-		return Arrays.asList(cliente1, cliente2);
-		
+		return clienteRepository.findAll();
 	}
 }
 
