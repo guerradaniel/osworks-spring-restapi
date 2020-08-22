4492bc91b5a98809b9ccfeb8d9f499650f037734
Daniel Guerra <danielguerra.g12@gmail.com>
2020-08-22 01:52:28 -0300

    Fiz o mapeamento das entidades da Classe Cliente.

--- src/main/java/com/algaworks/osworks/domain/models/Cliente.java
+++ src/main/java/com/algaworks/osworks/domain/models/Cliente.java
@@ -1,13 +1,22 @@
 package com.algaworks.osworks.domain.models;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Cliente {
 	
-	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nome;
 	private String email;
-	private String telefone;
 	
+	@Column(name = "fone")
+	private String telefone;
 	
 	
 	
@@ -34,7 +43,6 @@ public class Cliente {
 		this.email = email;
 	}
 	
-	
 	public String getTelefone() {
 		return telefone;
 	}
@@ -42,5 +50,31 @@ public class Cliente {
 		this.telefone = telefone;
 	}
 
+	
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
 }
 
