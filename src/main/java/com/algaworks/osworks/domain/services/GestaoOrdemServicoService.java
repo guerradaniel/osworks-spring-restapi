eed4b4131f8f03a834c4255ce5ae72689909d0f2
Daniel Guerra <danielguerra.g12@gmail.com>
2020-08-24 17:15:16 -0300

    Criei o método adicionaComentario salvando o comentario no repositório.

--- src/main/java/com/algaworks/osworks/domain/services/GestaoOrdemServicoService.java
+++ src/main/java/com/algaworks/osworks/domain/services/GestaoOrdemServicoService.java
@@ -5,11 +5,13 @@ import java.time.OffsetDateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.algaworks.osworks.api.models.Comentario;
 import com.algaworks.osworks.domain.exception.NegocioException;
 import com.algaworks.osworks.domain.models.Cliente;
 import com.algaworks.osworks.domain.models.OrdemServico;
 import com.algaworks.osworks.domain.models.StatusOrdemServico;
 import com.algaworks.osworks.domain.repository.ClienteRepository;
+import com.algaworks.osworks.domain.repository.ComentarioRepository;
 import com.algaworks.osworks.domain.repository.OrdemServicoRepository;
 
 @Service
@@ -21,6 +23,9 @@ public class GestaoOrdemServicoService {
 	@Autowired
 	private ClienteRepository clienteRepository;
 	
+	@Autowired
+	private ComentarioRepository comentarioRepository;
+	
 	
 	public OrdemServico criar(OrdemServico ordemServico) {
 		Cliente cliente = clienteRepository.findById(ordemServico.getCliente().getId())
@@ -30,7 +35,18 @@ public class GestaoOrdemServicoService {
 		ordemServico.setStatus(StatusOrdemServico.ABERTA);
 		ordemServico.setDataAbertura(OffsetDateTime.now());
 
-		
 		return ordemServicoRepository.save(ordemServico);
+	}
+	
+	public Comentario adicionarComentario(Long ordemServicoId, String descricao) {
+		OrdemServico ordemServico = ordemServicoRepository.findById(ordemServicoId)
+				.orElseThrow(() -> new NegocioException("Ordem de Serviço não encontrada"));
+		
+		Comentario comentario = new Comentario();
+		comentario.setDataEnvio(OffsetDateTime.now());
+		comentario.setDescricao(descricao);
+		comentario.setOrdemServico(ordemServico);
+		
+		return comentarioRepository.save(comentario);
 	}
 }
