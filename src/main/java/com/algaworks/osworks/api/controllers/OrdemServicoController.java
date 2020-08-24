55392c5a2724a2cc220ee13f7ffc6998d870b19e
Daniel Guerra <danielguerra.g12@gmail.com>
2020-08-24 04:18:45 -0300

    Fiz Representation Model (OrdemServicoModel) e setei modelmapper criando estratégia de correspondência.

--- src/main/java/com/algaworks/osworks/api/controllers/OrdemServicoController.java
+++ src/main/java/com/algaworks/osworks/api/controllers/OrdemServicoController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import javax.validation.Valid;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.algaworks.osworks.api.models.OrdemServicoModel;
 import com.algaworks.osworks.domain.models.OrdemServico;
 import com.algaworks.osworks.domain.repository.OrdemServicoRepository;
 import com.algaworks.osworks.domain.services.GestaoOrdemServicoService;
@@ -30,6 +32,9 @@ public class OrdemServicoController {
 	@Autowired
 	private OrdemServicoRepository ordemServicoRepository;
 	
+	@Autowired
+	private ModelMapper modelMapper;
+	
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
 	public OrdemServico criar(@Valid @RequestBody OrdemServico ordemServico) {
@@ -43,11 +48,13 @@ public class OrdemServicoController {
 	}
 	
 	@GetMapping("/{ordemServicoId}")
-	public ResponseEntity<OrdemServico> buscar(@PathVariable Long ordemServicoId){
+	public ResponseEntity<OrdemServicoModel> buscar(@PathVariable Long ordemServicoId){
 		Optional<OrdemServico> ordemServico = ordemServicoRepository.findById(ordemServicoId);
 		
-		if(ordemServico.isPresent()) 
-			return ResponseEntity.ok(ordemServico.get());
+		if(ordemServico.isPresent()) {
+			OrdemServicoModel ordemServicoModel = modelMapper.map(ordemServico.get(), OrdemServicoModel.class);
+			return ResponseEntity.ok(ordemServicoModel);
+		}
 		
 		return ResponseEntity.notFound().build();
 	}
